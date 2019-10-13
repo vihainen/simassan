32798d665b1e46ae1d8527345255a605ff2125c4
Gabriel Simões e Silva <Gabriel.Simoes.eSilva@gmail.com>
2019-10-13 10:12:38 -0300

    Create picker.js

---
+++ src/picker.js
@@ -0,0 +1,27 @@
+async function parse(text) {
+  const cmd = /\(pick\s([^)]*)\)/gi
+
+  const matches = []
+  let match
+  while ((match = cmd.exec(text)) !== null) {
+    const param = match[1].trim()
+    
+    const pattern = /(?:(\d*)"([^"]*)")|([^\s]+)/gi
+    let opts = []
+    while ((match = pattern.exec(param)) !== null) {
+      const weight = +match[1]||1
+      const opt = match[2]||match[3]
+      opts = [...opts, ...Array(weight).fill(opt)]
+      console.log(weight, opt)
+    }
+    
+    console.log(opts)
+    matches.push(['pick', opts[Math.floor(Math.random()*opts.length)]])
+    
+    match = ''
+  }
+
+  return matches
+}
+
+module.exports = parse
