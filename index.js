cdbec2331573ad47ed5b6bdefacf6b34847fe982
Gabriel Simões <gabriel.simoes@natahouse.com>
2022-08-29 16:57:15 -0300

    refactor: index "send" functions

--- index.js
+++ index.js
@@ -4,47 +4,58 @@ const { json } = require('micro')
 const run = require('./src/run')
 
 const telegram = new Telegram(process.env.BOT_KEY)
-function reply(chat, message_id, text) {
-  telegram.sendMessage(chat, text, {
-    reply_to_message_id: message_id,
-    parse_mode: 'Markdown'
-  })
-}
 
 function view(from, to) {
   return `${from} ↝ ${to}`
 }
 
-function sendPM(user, message_id, from, to) {
+function sendReply(chat, message_id, text) {
+  telegram.sendMessage(chat, text, {
+    reply_to_message_id: message_id,
+  })
+}
+
+function sendPM(user, from, to) {
   telegram.sendMessage(user, to)
   return view(from, 'I have sent you the requested information in a private message.')
 }
 
-function sendAdmin(message) {
+function sendToAdmin(message) {
   telegram.sendMessage(process.env.ADMIN_ID, message)
 }
 
 function sendError(data, error) {
-  sendAdmin(`Error!\n${view(data, error)}`)
+  sendToAdmin(`Error!\n${view(data, error)}`)
 }
 
 function getResponses(message) {
   return function ([from, to, type = 'MSG']) {
     switch(type) {
-      case 'PM':
-        return sendPM(message.from.id, message.message_id, from, to)
-      case 'REPLYREPLY':
-        const replyMessage = message.reply_to_message?.message_id
-        if (replyMessage) reply(message.chat.id, replyMessage, view(from, to))
-        return false
-      case 'ADM':
-        return sendAdmin(view(from, to))
-      case 'ERR':
-        return sendError(from, to)
       case 'MSG':
         return view(from, to)
+      case 'PM':
+        return sendPM(message.from.id, from, to)
+      case 'REPLYREPLY':
+        const replyMessage = message.reply_to_message?.message_id
+        if (replyMessage) sendReply(message.chat.id, replyMessage, view(from, to))
+        break;
+      case 'ADM':
+        sendToAdmin(view(from, to))
+        break;
+      case 'ERR':
+        sendError(from, to)
+        break;
     }
+
+    return false
   }
+}
+
+function sendMarkdownMessage(chat, message_id, text) {
+  telegram.sendMessage(chat, text, {
+    reply_to_message_id: message_id,
+    parse_mode: 'Markdown'
+  })
 }
 
 async function handler(request, response) {
@@ -55,7 +66,7 @@ async function handler(request, response) {
     if (values.length !== 0) {
       const responses = values.map(getResponses(message)).filter(Boolean)
 
-      reply(message.chat.id, message.message_id, responses.join('\n'))
+      sendMarkdownMessage(message.chat.id, message.message_id, responses.join('\n'))
     }
   }
 
